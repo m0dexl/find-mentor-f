ed5940e2958a09c69a14097a55120fe4b7358293
m0dexl <yahya.altungul@gmail.com>
2023-10-04 16:28:35 +0300

    icons fix

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faTwitter,
-  faFacebook,
-  faInstagram,
-} from '@fortawesome/free-brands-svg-icons';
-library.add(faTwitter, faFacebook, faInstagram);
-
+import { faInstagram,faTwitter,faFacebook } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  faInstagram = faInstagram;
+  faTwitter = faTwitter;
+  faFacebook = faFacebook;
 }
