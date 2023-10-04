1e32f21ffa6cf7236c426442f799d8296430cc69
m0dexl <yahya.altungul@gmail.com>
2023-10-04 13:27:36 +0300

    modulets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import { HeroComponent } from './home/hero/hero.component';
+import { HowitworksComponent } from './home/howitworks/howitworks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to home page
+  {path:"home",component:HomeComponent,children: [{path:"",component:HeroComponent},{path:"",component:HowitworksComponent}]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
