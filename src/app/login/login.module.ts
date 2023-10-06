b3986b15192ba10e22190aaff1f19a4d17829cfc
Özge <142818764+ozgekaraca@users.noreply.github.com>
2023-10-06 16:01:04 +0300

    signup-login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { HomeModule } from '../home/home.module';
+import { SignupComponent } from './signup/signup.component';
 
 
 
 @NgModule({
   declarations: [
     LoginComponent,
+    SignupComponent,
   
   ],
   imports: [
