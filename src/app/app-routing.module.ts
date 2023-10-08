67efd0cc675769734a3038217045dcbe23792a63
burcuersoy <ersoyburcu@icloud.com>
2023-10-08 16:51:09 +0300

    form routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { CategoriesComponent } from './category/categories/categories.component'
 import { LoginComponent } from './login/login/login.component';
 import { AdsComponent } from './ads/ads/ads.component';
 import { SignupComponent } from './login/signup/signup.component';
+import { FormComponent } from './ads/form/form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to home page
@@ -16,7 +17,8 @@ const routes: Routes = [
   {path: "categories",component:CategoriesComponent},
   {path: "ads",component:AdsComponent},
   {path: "login", component:LoginComponent},
-  {path: "signup", component: SignupComponent}
+  {path: "signup", component: SignupComponent},
+  {path: "form", component:FormComponent}
 ];
 
 @NgModule({
