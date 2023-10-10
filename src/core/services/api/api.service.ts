4dccbfb441003d1c1fb76ba10c04af0a1e2216f6
m0dexl <yahya.altungul@gmail.com>
2023-10-10 11:45:22 +0300

    api service login register

--- src/core/services/api/api.service.ts
+++ src/core/services/api/api.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, map, share } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { LoginRequest } from 'src/core/models/request/login-request-model';
+import { BaseDataResponse } from 'src/core/models/response/base-data-response-model';
+import { TokenResponse } from 'src/core/models/response/token-response-model';
+import { RegisterRequest } from 'src/core/models/request/register-request-model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private endpoint = environment.api_url;
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) { }
+
+  login(request: LoginRequest): Observable<BaseDataResponse<TokenResponse>> {
+    return this.http
+    .post<BaseDataResponse<TokenResponse>>(
+      this.endpoint + "/Auth/Login",
+      request
+    )
+    .pipe(
+      map((result) => {
+        return result;
+      })
+    )
+  }
+
+  register(request: RegisterRequest):Observable<BaseDataResponse<TokenResponse>>{
+    return this.http
+    .post<BaseDataResponse<TokenResponse>>(
+      this.endpoint + "/Auth/Register",
+      request
+    )
+    .pipe(
+      map((result) => {
+        return result;
+      })
+    )
+  }
 }
