1d00cd0887ea40de0a8ced4c3963b4504a6d4b59
burcuersoy <ersoyburcu@icloud.com>
2023-10-11 16:41:24 +0300

    auth.service jwt.interceptor info

--- src/core/services/auth/auth.service.ts
+++ src/core/services/auth/auth.service.ts
@@ -12,51 +12,75 @@ import { RegisterRequest } from 'src/core/models/request/register-request-model'
   providedIn: 'root'
 })
 export class AuthService {
+  // Kullanıcının mevcut durumunu temsil eden Observable.
   public currentUser: Observable<User | null>;
+  // Kullanıcının mevcut durumunu saklayan özel BehaviorSubject.
   private currentUserSubject: BehaviorSubject<User | null>;
 
-  constructor(private readonly apiService: ApiService, private router: Router) {
+  constructor(private readonly apiService: ApiService, private router: Router) { //(API çağrıları yapmak için kullanılır.)
+     //Bu kod satırı, AuthService sınıfının kurucu fonksiyonunda yer alır 
+    // kullanıcının mevcut oturum durumunu saklamak için kullanılan bir BehaviorSubject nesnesini başlatır.
     this.currentUserSubject = new BehaviorSubject<User | null>(JSON.parse(<string>sessionStorage.getItem('current_user')))
-    this.currentUser = this.currentUserSubject.asObservable();
+    //(currentUserSubject, sessionStorage'da saklanan mevcut kullanıcı bilgisini tutar.)
+    this.currentUser = this.currentUserSubject.asObservable();//kullanıcının mevcut oturum durumunu izlemek ve diğer bileşenlerle paylaşmak için kullanılır. 
   }
 
-  public get currentValue(): User | null{
+  public get currentValue(): User | null{ //kullanıcının mevcut durumunu okuma işlemi
     return this.currentUserSubject.value;
   }
 
+ // Kullanıcı girişi işlemi
+  //Kullanıcıların oturum açma isteklerini işlerken, alınan yanıtları ve kullanıcı bilgilerini günceller.
   public async login(request: LoginRequest): Promise<ResponseStatus> {
-    const loginResponse = await this.apiService.login(request).toPromise();
+    const loginResponse = await this.apiService.login(request).toPromise();// API üzerinden kullanıcı girişini yapar.
 
+   //oturum açma işleminin sonucunu status adlı bir değişkende saklar ve bu değişken daha sonra oturum açma işleminin sonucunu değerlendirmek için kullanılır.
     let status = loginResponse!.status;
 
+  //oturum açma işleminin sonucunu kontrol etmek ve oturumun başarılı olup olmadığını belirlemek için kullanılır.
     if(status == ResponseStatus.Ok) {
-      this.setToken(loginResponse!.data);
+      this.setToken(loginResponse!.data); //oturum açma işlemi başarılıysa oturum açma işlemi sonucunda elde edilen token bilgisini saklar.
+  
 
-      const profileResponse = await this.apiService
+      const profileResponse = await this.apiService // kullanıcının profil bilgilerini sunucudan almak için bir API çağrısı yapar ve bu bilgileri profileResponse değişkeninde saklar. 
       .getProfileInfo()
-      .toPromise();
+      .toPromise(); //kullanıcının profil bilgilerini getirmek için başlatılan Observable'ın sonucunu bir Promise'e dönüştürür. 
+      //bu, profil bilgilerini almak ve bu bilgileri işlemek için kullanılır. 
 
-      status = profileResponse!.status;
+      status = profileResponse!.status; //profil bilgileri alma işleminin sonucunu status adlı bir değişkende saklar 
+  
 
-      if(status == ResponseStatus.Ok){
+      if(status == ResponseStatus.Ok){ //profil bilgilerini alma işleminin sonucunu kontrol eder ve eğer bu işlem başarılı ise, belirtilen kod bloğunu çalıştırır. 
+
+        //Başarılı bir şekilde profil bilgilerini alındığında, bu bilgileri kullanıcının oturum süresince saklamak amacıyla sessionStorage içine kaydeder.
+        //Bu, kullanıcı oturum sırasında bu bilgilere ihtiyaç duyulduğunda kolayca erişilebilmesini sağlar.
         sessionStorage.setItem('current_user', JSON.stringify(profileResponse!.data))
+
+        //Kullanıcının oturum bilgilerini güncellemek ve bu güncellenmiş bilgileri diğer bileşenlerle paylaşmak için kullanılır.
+        //Özellikle, kullanıcı oturum açtığında veya oturumu kapattığında bu kod parçası kullanılır. 
         this.currentUserSubject.next(profileResponse!.data);
       } else{
-        await this.logOut();
+        await this.logOut(); //oturum açma başarısızsa oturumu sonlandırır.
       }
     }
-    return status;
+    return status; //login fonksiyonun sonucunu temsil eden bir değeri döndürür. 
+    //Bu değer, çağrıldığı yerde işlemin başarılı olup olmadığını veya olası hataları belirlemek için kullanılır.
   }
 
+  //Bu fonksiyon, bir kayıt isteği alır ve bu isteği işleyerek bir ResponseStatus değeri içeren bir Promise döndürür.(register fonksiyonunun tanımını ve imzasını oluşturur. )
   public async register(request: RegisterRequest): Promise<ResponseStatus> {
 
 
+    //kullanıcının kayıt işlemini başlatır ve bu işlemin sonucunu registerResponse değişkeninde saklar.
+    //Bu, kayıt işleminin başarı durumunu ve alınan cevap verilerini işlemek için kullanılır.
     const registerResponse = await this.apiService.register(request).toPromise();
   
-    let status = registerResponse!.status;
+    let status = registerResponse!.status; //kayıt işleminin sonucunu temsil eden işlem durumunu status adlı bir değişkende saklar. 
   
-    if (status == ResponseStatus.Ok) {
-      this.setToken(registerResponse!.data);
+    if (status == ResponseStatus.Ok) { // İşlem durumu 'Ok' olduğunda (kayıt başarılı ise) bu blok çalışır.
+      this.setToken(registerResponse!.data);// Oturum bilgilerini güncelle
+
+      // Kullanıcının oturumunu kapat ve oturum bilgilerini temizle
       sessionStorage.setItem('current_user', JSON.stringify({}));
       this.currentUserSubject.next({} as User);
     }
@@ -64,31 +88,32 @@ export class AuthService {
     return status;
   }
   
-    public async refreshToken(): Promise<boolean> {
-      const refreshTokenResponse = await this.apiService
-        .refreshToken(<string>sessionStorage.getItem('refresh_token'))
+    public async refreshToken(): Promise<boolean> { // Oturum yenileme işlemi başlar.
+      const refreshTokenResponse = await this.apiService // refreshTokenResponse adlı bir değişken oluştur ve oturum yenileme isteğini yap
+        .refreshToken(<string>sessionStorage.getItem('refresh_token')) // Mevcut tarayıcı oturumunun yenileme belirtecini alır
         .toPromise();
   
-      if (refreshTokenResponse!.status == ResponseStatus.Ok) {
-        this.setToken(refreshTokenResponse!.data);
+      if (refreshTokenResponse!.status == ResponseStatus.Ok) {  // Oturum yenileme işleminin sonucunu kontrol et
+        this.setToken(refreshTokenResponse!.data); // Yenilenmiş oturum bilgilerini ayarla
   
-        return true;
+        return true; // İşlem başarılı, true döndür
       }
   
-      return false;
+      return false; // İşlem başarısız, false döndür
     }
 
 
-  private setToken(token: TokenResponse | null) {
-    if(token != null){
-      sessionStorage.setItem('access_token',JSON.stringify(token.accessToken));
-      sessionStorage.setItem('token_expiration', JSON.stringify(token.expiration));
-      sessionStorage.setItem('refresh_token', JSON.stringify(token.refreshToken));
+  private setToken(token: TokenResponse | null) { // Oturum bilgilerini saklama işlemi başlar.
+    if(token != null){  // Eğer gelen oturum bilgileri (token) null değilse, bu bloğa girilir.
+       
+      sessionStorage.setItem('access_token',JSON.stringify(token.accessToken)); // Erişim belirtecini tarayıcı oturumu içinde sakla
+      sessionStorage.setItem('token_expiration', JSON.stringify(token.expiration)); // Belirtecin son kullanma süresini sakla
+      sessionStorage.setItem('refresh_token', JSON.stringify(token.refreshToken));   // Yenileme belirtecini tarayıcı oturumu içinde sakla
     }
   }
 
-  async logOut() {
-    sessionStorage.clear();
-    this.currentUserSubject.next(null);
+  async logOut() {   // Oturum kapatma işlemi başlar.
+    sessionStorage.clear(); // Tarayıcı oturumu içindeki tüm verileri temizle
+    this.currentUserSubject.next(null);  // Kullanıcının oturumunu sonlandır ve değeri null olarak ayarla
   }
 }
--- src/core/services/interceptor/jwt.interceptor.ts
+++ src/core/services/interceptor/jwt.interceptor.ts
@@ -10,38 +10,57 @@ import { environment } from '../../../environments/environment';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable()
-export class JwtInterceptor implements HttpInterceptor {
+
+//JwtInterceptor adında bir sınıf tanımlar ve bu sınıf, HttpInterceptor arayüzünü uygular. 
+//HttpInterceptor, HTTP isteklerini ele almak ve düzenlemek için kullanılan bir Angular arayüzüdür.
+export class JwtInterceptor implements HttpInterceptor { 
+
+//JwtInterceptor sınıfı, HTTP isteklerini ele alarak oturum yönetimi ve belirteç işlemlerini gerçekleştirmek için AuthService adındaki bir servisi kullanır.
+//Bu işlem, oturum belirtecini kontrol etmek ve gerektiğinde yenilemek gibi görevleri içerebilir.
   constructor(private authService: AuthService) {}
 
   //prettier-ignore
+  //intercept fonksiyonu, HttpInterceptor arayüzünün bir parçasıdır ve HTTP isteklerini ele almak için kullanılır.
   intercept(
-    request: HttpRequest<any>,
+    request: HttpRequest<any>,  // Gelen HTTP isteği
     next: HttpHandler
-  ): Observable<HttpEvent<any>> {
+  ): Observable<HttpEvent<any>> { //HTTP isteği sonucunu temsil eden Observable
+
+    //Bu satır, gelen isteğin URL'sinin, belirli bir environment.api_url ile başlayıp başlamadığını ve aynı zamanda "RefreshToken" kelimesini içerip içermediğini kontrol eder. 
+    //Bu, yalnızca belirli bir URL desenine sahip isteklerin işleneceğini belirtir.
     if (request.url.startsWith(environment.api_url) && !request.url.includes('RefreshToken')
     ) {
-      const expiration = Date.parse(JSON.parse(<string>sessionStorage.getItem('token_expiration')));
+      //Oturum belirtecinin son kullanma tarihini alır.
+      //Oturum belirtecinin son kullanma tarihi, sessionStorage içinde saklanır ve Date.parse ile bir tarihe dönüştürülür.
+        const expiration = Date.parse(JSON.parse(<string>sessionStorage.getItem('token_expiration')));
 
-      if (expiration != null && Date.now() > expiration) {
-        this.authService.refreshToken().then((result) => {
-          if (result) {
-            request = this.setRequest(request);
-          } else {
-            this.authService.logOut();
+      if (expiration != null && Date.now() > expiration) { //Oturum belirtecinin son kullanma tarihi (expiration) dolmuşsa ve hâlâ geçerliyse bu bloğa girilir.
+        this.authService.refreshToken().then((result) => { //Oturum belirteci yeniden alınmaya çalışılır. refreshToken fonksiyonu bir Promise döndürür.
+
+          if (result) {  // Eğer refreshToken başarılı olduysa (result true ise) bu bloğa girilir.
+
+            request = this.setRequest(request); // İstek, oturum belirteci eklenmiş haliyle güncellenir.
+          } else {  // Eğer refreshToken başarısız olduysa (result false ise) bu bloğa girilir.
+            this.authService.logOut(); // Kullanıcı oturumdan çıkarılır (logOut fonksiyonu kullanılır).
           }
         });
       }
 
-      request = this.setRequest(request);
+      request = this.setRequest(request); //Bu satır, request değişkenini this.setRequest(request) fonksiyonunun dönüş değeri ile günceller. 
     }
     
-    return next.handle(request);
+    return next.handle(request); //İşlenmiş HTTP isteğini sonraki adıma (interceptor'a veya HTTP isteğinin nihai hedefine) iletmek için kullanılır.
   }
 
   //prettier-ignore
+  // Verilen HTTP isteğini klonlayarak orijinal isteği değiştirmemek ve yeni bir istek oluşturmak için kullanıyoruz.
   private setRequest(request: HttpRequest<any>): HttpRequest<any> {
-    return request.clone({
-      setHeaders: {
+    return request.clone({  // Oluşturulan isteği düzenlemek için kullanılan bir HttpRequest metodu.
+      setHeaders: { // İsteğin başlıklarını düzenlemek için kullanılır.
+
+      // Authorization başlığı, oturum belirteci ile kimlik doğrulamasını sunucuya sağlayan önemli bir başlık.
+      // Bearer yöntemiyle sunucuya oturum belirteci (token) ile kimlik doğrulama sağlanır.
+      // sessionStorage.getItem('access_token') ile oturum belirteci alınır ve Authorization başlığına eklenir.
         Authorization: `Bearer ${JSON.parse(<string>sessionStorage.getItem('access_token'))}`,
       },
     });
