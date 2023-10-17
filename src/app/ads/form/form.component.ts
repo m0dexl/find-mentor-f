150cc9302bfd06c79262e7f6d64928861987c670
burcuersoy <ersoyburcu@icloud.com>
2023-10-17 09:56:53 +0300

    form css ve backend bağlantısı

--- src/app/ads/form/form.component.ts
+++ src/app/ads/form/form.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/core/services/api/api.service'; 
+import { Router } from '@angular/router';
+import {Answer} from 'src/core/models/answer.model'
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent {
+export class FormComponent implements OnInit{
+
+  constructor(private ApiService: ApiService, 
+    private router: Router) {
+   
+  }
+
+  answer: Answer[] = [];
+
+  ngOnInit() {  // Bileşen başlatıldığında otomatik olarak "refresh" fonksiyonunu çağırıyoruz.
+    this.refresh();
+  }
+
+  refresh() {  
+    this.ApiService.getAllEntities(Answer).subscribe((response) => {     
+      this.answer = response.data;     
+      console.log(this.answer); 
+    });
+  }
+
+  
 
 }
