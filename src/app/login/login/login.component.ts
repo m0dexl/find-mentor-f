d34aec01e123520f90eaf15c844c892ab35aa448
Özge <142818764+ozgekaraca@users.noreply.github.com>
2023-10-18 00:28:04 +0300

    login page

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { ConfirmationService, MessageService } from 'primeng/api';
+import { MessageService } from 'primeng/api';
 import { LoginRequest } from 'src/core/models/request/login-request-model';
 import { ResponseStatus } from 'src/core/models/response/base-response-model';
 import { AuthService } from 'src/core/services/auth/auth.service';
@@ -9,29 +9,29 @@ import { AuthService } from 'src/core/services/auth/auth.service';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  providers:[MessageService, ConfirmationService]
+  providers: [MessageService]
 })
-export class LoginComponent{
-
+export class LoginComponent {
   public loginRequest: LoginRequest = <LoginRequest>{};
 
   constructor(
     private readonly authService: AuthService,
     private readonly router: Router,
     private messageService: MessageService
-  ){}
+  ) {}
 
-  async login (){
+  async login() {
     let status = await this.authService.login(this.loginRequest);
 
-    if(status == ResponseStatus.Ok){
-      await this.router.navigate(['profile']);
-    }else if(status == ResponseStatus.Invalid){
+    if (status === ResponseStatus.Ok) {
+      await this.router.navigate(['../profile']);
+    } else if (status === ResponseStatus.Invalid) {
       this.messageService.add({
-        severity: 'error', 
-        summary:'Error', 
-        detail: 'Email veya şifre hatalı'});
-      this.loginRequest.password='';
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Kullanıcı adı veya şifre hatalı',
+      });
+      this.loginRequest.Password = '';
     }
   }
 }
