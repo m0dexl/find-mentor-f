56dc1fea260f4136c1f328637c7ca8dd0e81271e
Özge <142818764+ozgekaraca@users.noreply.github.com>
2023-10-20 10:34:39 +0300

    login alert fix

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,5 +1,7 @@
+
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { LoginRequest } from 'src/core/models/request/login-request-model';
 import { ResponseStatus } from 'src/core/models/response/base-response-model';
 import { AuthService } from 'src/core/services/auth/auth.service';
@@ -8,27 +10,30 @@ import { AuthService } from 'src/core/services/auth/auth.service';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  providers: [MessageService]
 })
 export class LoginComponent {
   public loginRequest: LoginRequest = <LoginRequest>{};
 
   constructor(
     private readonly authService: AuthService,
-    private readonly router: Router
+    private readonly router: Router,
+    private messageService: MessageService
   ) {}
 
+  
   async login() {
     let status = await this.authService.login(this.loginRequest);
 
     if (status == ResponseStatus.Ok) {
       await this.router.navigate(['../profile']);
     } else {
-      const errorMessage =
-        'Hatalı giriş yaptınız. Kullanıcı adınızı veya şifrenizi kontrol ediniz.';
-        
-      if (window.confirm(errorMessage)) {
-        this.loginRequest.password = ''; 
-      }     
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Hata',
+        detail: 'Kullanıcı adı veya şifre hatalı!'});
+         this.loginRequest.password = '';
     }
   }
+  
 }
