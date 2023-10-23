fa8bca85c3e0bd0f3d7138f9f202ebe95266649e
Özge <142818764+ozgekaraca@users.noreply.github.com>
2023-10-23 17:20:44 +0300

    profilde log out yapma

--- src/app/profile/profile-content/profile-content.component.ts
+++ src/app/profile/profile-content/profile-content.component.ts
@@ -3,51 +3,48 @@ import { Router } from '@angular/router';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { User } from 'src/core/models/user.model';
 import { ApiService } from 'src/core/services/api/api.service';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-profile-content',
   templateUrl: './profile-content.component.html',
-  styleUrls: ['./profile-content.component.scss']
+  styleUrls: ['./profile-content.component.scss'],
 })
 export class ProfileContentComponent implements OnInit {
+  faRightFromBracket = faRightFromBracket;
 
-  constructor(private apiService: ApiService, 
-    private router: Router) {
-  }
+  constructor(private apiService: ApiService, private router: Router) {}
   ngOnInit() {
     this.getCurrentUser();
     this.firstName = this.getName(this.currentUser.fullName);
     this.lastName = this.getLastname(this.currentUser.fullName);
   }
 
-  currentUser : User = <User> {};
-  firstName: string = "";
-  lastName: string = "";
-  
+  currentUser: User = <User>{};
+  firstName: string = '';
+  lastName: string = '';
+
   faEdit = faEdit;
 
-  getCurrentUser(){
+  getCurrentUser() {
     const userJson = sessionStorage.getItem('current_user');
     this.currentUser = userJson !== null ? JSON.parse(userJson) : new User();
   }
 
-  getName(fullName: string){
+  getName(fullName: string) {
     const words = fullName.split(' ');
     words.pop();
     return words.join(' ');
   }
 
-  getLastname(fullName: string){
+  getLastname(fullName: string) {
     const words = fullName.split(' ');
     return words[words.length - 1];
   }
-  
 
-  inputUsername:boolean = false;
+  inputUsername: boolean = false;
 
-
-  showInputUsername(){
+  showInputUsername() {
     this.inputUsername = !this.inputUsername;
   }
-
 }
