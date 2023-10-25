8cb186a2a1960e2d0972bfc65759c4c5face4695
m0dexl <yahya.altungul@gmail.com>
2023-10-25 22:18:40 +0300

    login alert fix

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -24,19 +24,17 @@ export class LoginComponent {
     let status = await this.authService.login(this.loginRequest);
 
     if (status == ResponseStatus.Ok) {
-      const userType = this.authService.getUserType();
-      if (userType === 0) {
-        await this.router.navigate(['/admin']); // Admin ise
-      } else if (userType === 1) {
-        await this.router.navigate(['/profile']); // Mnetor ise
-      }
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Başarılı',
+        detail: 'Giriş yapılıyor. Hoşgeldiniz!',
+      });
     } else {
       this.messageService.add({
         severity: 'error',
         summary: 'Hata',
         detail: 'Kullanıcı adı veya şifre hatalı!',
       });
-      this.loginRequest.password = '';
     }
   }
 }
