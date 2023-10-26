66e2dd54fe8d033f5f6b1f76e715258a949e436f
burcuersoy <ersoyburcu@icloud.com>
2023-10-26 19:50:49 +0300

    admin

--- src/app/admin/admin-content/admin-content.component.ts
+++ src/app/admin/admin-content/admin-content.component.ts
@@ -5,6 +5,8 @@ import { User } from 'src/core/models/user.model';
 import { ResponseStatus } from 'src/core/models/response/base-response-model';
 import { RegisterRequest } from 'src/core/models/request/register-request-model';
 import { AuthService } from 'src/core/services/auth/auth.service';
+import { Notice } from 'src/core/models/notice.model';
+import { Category } from 'src/core/models/category.model';
 
 @Component({
   selector: 'app-admin-content',
@@ -17,13 +19,55 @@ export class AdminContentComponent implements OnInit {
 
   ngOnInit() {
     this.showMentor();
+    this.getNotice();
+    this.getUser();
+    this.getCategories();
   }
 
   users: User[] = [];
+  notices: Notice[] = [];
+  usercategories: Category[] = [];
 
   newUserName: string = ''; // yeni kullanıcı eklemek için
   newFullName: string = '';
   newEmail: string = '';
+  categoryCount: number = 0;
+  userCount: number = 0;
+  noticeCount: number = 0;
+  
+  getNotice() {
+    this.apiService.getAllEntities(Notice).subscribe((response) => {
+      this.notices = response.data;
+      this.noticeCount = this.notices.length; //kaç ilan varsa
+    });
+  }
+  getUser() {
+    this.apiService.getAllEntities(User).subscribe((response) => {
+      const allUsers = response.data;
+      const userType1Users = allUsers.filter((user) => user.userType === 1); //user typeı 1 olan mentörleri getir
+      this.userCount = userType1Users.length;
+    });
+  }
+
+  getCategories() {
+    this.apiService.getAllEntities(Category).subscribe((response) => {
+      this.usercategories = response.data;
+      this.categoryCount = this.usercategories.length; //kaç kategori varsa
+
+      this.usercategories = this.usercategories.map((category) => {
+        return {
+          ...category,
+          category_Name:
+            category.category_Name.charAt(0).toUpperCase() +
+            category.category_Name.slice(1),
+          category_Description:
+            category.category_Description.charAt(0).toUpperCase() +
+            category.category_Description.slice(1),
+        };
+      });
+    });
+  }
+
 
   showMentor() {
     this.apiService.getAllEntities(User).subscribe((response) => {
